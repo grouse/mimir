a6c36dd0766ab0a74f985465d7e0e16a4bce8394
Jesper Stefansson <jesper.stefansson@gmail.com>
2025-02-22 12:35:59 +0100

    generated files

--- src/generated/internal/mimir.h
+++ src/generated/internal/mimir.h
@@ -4,6 +4,7 @@
 static u32 hash32(BufferId buffer, u32 seed = MURMUR3_SEED);
 static void ts_parse_buffer(Buffer *buffer, TSInputEdit edit);
 static void lsp_open(LspConnection *lsp, BufferId buffer_id, String language_id, String content);
+static i32 line_from_offset(i64 offset, Array<i64> offsets, i32 guessed_line = 0);
 static void app_gather_input(AppWindow *wnd);
 static void update_and_render();
 
